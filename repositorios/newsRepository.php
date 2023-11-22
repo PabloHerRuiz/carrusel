0fe36a0d4ac127c26f71ba4e8f62544e116ae619
PabloHerRuiz <pherrui680@g.educaand.es>
2023-11-22 12:06:06 +0100

    repositorio hecho

--- repositorios/newsRepository.php
+++ repositorios/newsRepository.php
@@ -11,13 +11,40 @@ class NewsRepository
 
 
     //CREAR
+    public function createNews($f_inicio, $f_fin, $duracion, $prioridad, $titulo, $perfil, $tipo)
+    {
+        $query = "INSERT INTO NOTICIAS (f_inicio,f_fin,duracion,prioridad,titulo,perfil,tipo) VALUES (:f_inicio,:f_fin,:duracion,:prioridad,:titulo,:perfil,:tipo)";
+        $stmt = $this->conexion->prepare($query);
+        $stmt->bindParam(":f_inicio", $f_inicio, PDO::PARAM_STR);
+        $stmt->bindParam(":f_fin", $f_fin, PDO::PARAM_STR);
+        $stmt->bindParam(":duracion", $duracion, PDO::PARAM_INT);
+        $stmt->bindParam(":prioridad", $prioridad, PDO::PARAM_INT);
+        $stmt->bindParam(":titulo", $titulo, PDO::PARAM_STR);
+        $stmt->bindParam(":perfil", $perfil, PDO::PARAM_STR);
+        $stmt->bindParam(":tipo", $tipo, PDO::PARAM_STR);
+        $stmt->execute();
+    }
 
     //BORRAR
-
+    public function deleteNew($idNoticia)
+    {
+        $query = "DELETE FROM NOTICIAS WHERE IDNOTICIA =:idNoticia";
+        $stmt = $this->conexion->prepare($query);
+        $stmt->bindParam(":idNoticia", $idNoticia, PDO::PARAM_INT);   
+        $stmt->execute();
+    }
     //UPDATE
 
     //LEER
-
+    public function readNew($idNoticia)
+    {
+        $query = "SELECT * FROM NOTICIAS WHERE IDNOTICIA =:idNoticia";
+        $stmt = $this->conexion->prepare($query);
+        $stmt->bindParam(":idNoticia", $idNoticia, PDO::PARAM_INT);   
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
 }
 
 
